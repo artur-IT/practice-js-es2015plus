3c32d6552f21f16fc4fc54de0adc93755599c145
Artur Matysiak <inz.matys@gmail.com>
2023-12-14 19:24:33 +0100

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,18 @@
-import {files} from './data.js';
+import { files } from "./data.js";
+let sum = 0;
 
+const dataKB = (number) => number * 1024;
+
+const dataMB = (number) => number * 1024 * 1024;
+
+const dataGB = (number) => number * 1024 * 1024 * 1024;
+
+files.forEach((item) => {
+  const { unit, length } = item.size;
+  unit == "undefined" || !unit ? (sum += length) : null;
+  unit == "KB" ? (sum += dataKB(length)) : null;
+  unit == "MB" ? (sum += dataMB(length)) : null;
+  unit == "GB" ? (sum += dataGB(length)) : null;
+});
+
+console.log("razem:", sum, "Bajtów");
