1a21440ba5e36460063b2269850d056ecbd58b23
Artur Matysiak <inz.matys@gmail.com>
2023-12-18 13:52:27 +0100

    task done

--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,31 @@
-import Programmer from './classes/Programmer';
+import Programmer from "./classes/Programmer.js";
+import Junior from "./classes/Junior.js";
+import Mid from "./classes/Mid.js";
+import Senior from "./classes/Senior.js";
 
 const features = {
-    skills: 10, 
-    experience: 10, 
-    willingness: 10,
-}
+  skills: 10,
+  experience: 10,
+  willingness: 10,
+};
 
 const task = {
-    difficult: 1,
-    size: 1,
-}
+  difficult: 3,
+  size: 3,
+};
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
-console.log(time);
+// const programmer = new Programmer(features);
+// const time = programmer.getApproximateTimeTaskDoneInHours(task);
+// console.log("Programmer: ", time);
+
+const programmerJunior = new Junior(features);
+const timeJunior = programmerJunior.getApproximateTimeTaskDoneInHours(task);
+console.log("Junior: ", timeJunior);
+
+const programmerMid = new Mid(features);
+const timeMid = programmerMid.getApproximateTimeTaskDoneInHours(task);
+console.log("Mid: ", timeMid);
+
+const programmerSenior = new Senior(features);
+const timeSenior = programmerSenior.getApproximateTimeTaskDoneInHours(task);
+console.log("Senior: ", timeSenior);
